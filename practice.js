044b6fd81b5f106fe204b3cdbc1ce8fbab36fbdc
J H <jahalu88@hotmail.com>
2018-06-23 14:07:29 -0400

    practice anagrams

---
+++ practice.js
@@ -0,0 +1,12 @@
+// function to check if anagrams are equal
+
+function anagramCheck(x, y){
+    str = x.toLowerCase().replace(/\s/g, "").split("").sort().join("");
+    str2 = y.toLowerCase().replace(/\s/g, "").split("").sort().join("");
+      console.log(str, str2)
+    return str === str2;
+  }
+  
+  console.log(anagramCheck("Hello World", "dlroW olleH")); // => true
+  console.log(anagramCheck("Imtkheing'", "I'm the king")); // => true
+  console.log(anagramCheck("test", "tesd")); // => false
